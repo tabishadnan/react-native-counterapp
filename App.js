eb621bedb5d7c120352657b7a5146f7d2ee0ea34
tabishadnan <tabishadnan000@gmail.com>
2020-10-31 18:59:36 +0500

    react-native counter app

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text } from 'react-native';
 
 export default function App() {
+
+  const Separator = () => (
+    <View style={styles.separator} />
+  );
+
+  const [counter, setCounter] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>
+          Counter : {counter}
+      </Text>
+      </View>
+      <Separator />
+      <View>
+        <View style={styles.fixToText}>
+          <Button
+            title="INCREMENT"
+            onPress={() => setCounter(counter + 1)}
+          />
+          <Button
+            title="DECREMENT"
+            onPress={() => setCounter(counter - 1)}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
